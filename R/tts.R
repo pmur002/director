a4bff4fc52bc842b31d2839c0e024ede047b3a5d
Paul Murrell <paul@stat.auckland.ac.nz>
2020-04-21 13:19:37 +1200

    flesh out polly TTS

--- R/tts.R
+++ R/tts.R
@@ -9,15 +9,18 @@ TTS <- function(read, speak, ...) {
     x
 }
 
+#######################################
 ## Default TTS ignores any SSML markup and uses espeak
 defaultRead <- function(dialogue) {
     xml_text(dialogue)
 }
+
 defaultSpeak <- function(infile, outfile, ...) {
     system(paste("espeak -s 125 -v en -w", outfile,
                  "-f", infile))
     outfile    
 }
+
 defaultTTS <- TTS(read=defaultRead, speak=defaultSpeak)
 
 #######################################
@@ -32,7 +35,14 @@ defaultTTS <- TTS(read=defaultRead, speak=defaultSpeak)
 pollyRead <- function(dialogue) {
     as.character(xml_children(dialogue))
 }
-pollySpeak <- function(infile, outfile, voice="Matthew") {
+
+pollySpeak <- function(infile, outfile,
+                       voice="Matthew",
+                       engine="standard",
+                       format="mp3",
+                       sampleRate="",
+                       language=NULL,
+                       lexicons=NULL) {
     dialogue <- paste(readLines(infile), collapse="\n")
     ## Try to determine whether this is SSML
     if (grepl("^<speak>", gsub("^ *", "", dialogue))) {
@@ -40,15 +50,76 @@ pollySpeak <- function(infile, outfile, voice="Matthew") {
     } else {
         textType <- "text"
     }
-    mp3file <- tempfile()
+    audiofile <- tempfile()
+    if (is.null(lexicons)) {
+        lexiconArgs <- ""
+    } else {
+
+    }
+    if (is.null(language)) {
+        languageArg <- ""
+    } else {
+        languageArg <- paste0("--language-code ", language, " ")
+    }
+    if (nchar(sampleRate)) {
+        sampleRateArg <- paste0("--sample-rate ", sampleRate, " ")
+    } else {
+        sampleRateArg <- ""
+    }
     system(paste0("aws polly synthesize-speech ",
-                  "--output-format mp3 ",
+                  "--engine ", engine, " ",
+                  "--output-format ", format, " ",
                   "--voice-id ", voice, " ",
+                  sampleRateArg,
+                  languageArg,
                   "--text '", dialogue, "' ",
                   "--text-type ", textType, " ",
-                  mp3file))
-    ## mp3 to WAV
-    writeWave(prepComb(readMP3(mp3file), where="end"), outfile)
+                  audiofile))
+    if (format == "mp3") {
+        wav <- readMP3(audiofile)
+    } else {
+        pcm <- read_audio_bin(audiofile)
+        wav <- Wave(pcm, samp.rate=attr(pcm, "sample_rate"), pcm=TRUE)
+    }
+    writeWave(prepComb(wav, where="end"), outfile)
 }
-pollyTTS <- TTS(read=pollyRead, speak=pollySpeak, voice="Matthew")
-                
+
+## Does not allow format="json" OR speechMarks, but those COULD
+## be added to allow further processing on speech, such as
+## dynamic word highlighting on subtitles ???
+pollyTTS <- function(voice="Matthew",
+                     engine=c("standard", "neural"),
+                     format=c("mp3", "ogg_vorbis", "pcm"),
+                     sampleRate="default",
+                     ## Only relevant for bilingual voices 
+                     language=NULL,
+                     ## Special pronunciations
+                     lexicons=NULL) {
+    engine <- match.arg(engine)
+    format <- match.arg(format)
+    if (sampleRate == "default") {
+        sampleRate <- switch(format,
+                             mp3=,
+                             ogg_vorbis=switch(engine,
+                                               standard="22050",
+                                               neural="24000"),
+                             pcm="16000",
+                             "22050")
+    } else {
+        validSample <- switch(format,
+                              mp3=,
+                              ogg_vorbis=c("8000", "16000", "22050", "24000"),
+                              pcm=c("8000", "16000"))
+        if (format == "json") {
+            sampleRate <- ""
+        } else {
+            sampleRate <- as.character(sampleRate)
+            if (!(sampleRate %in% validSample))
+                stop("Invalid sample rate")
+        }
+    }
+    TTS(read=pollyRead, speak=pollySpeak,
+        voice=voice, engine=engine, format=format,
+        sampleRate=sampleRate,
+        language=language, lexicons=lexicons)
+}            
